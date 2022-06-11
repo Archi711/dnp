49e5f49d5d0633cf09750439c9fa1100f5feb349
Archi711 <mlody711@o2.pl>
2022-06-11 22:49:43 +0200

    feat - add v1

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,27 @@
 import Head from 'next/head'
 import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Desire | koncert 7.08</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin></link>
+        <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet"></link>
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
+        <Header title="DIZAJER" />
+        <Image className='image' src='/logo-synth.png' width={300} height={400} />
+        <p className='pt-4'>
+          gra koncert
         </p>
+        <p>na placu wolności</p>
+        <time dateTime='2022-07-22'>7 sierpnia 2022</time>
       </main>
-
-      <Footer />
     </div>
   )
 }
